77c5ebba375c78446baa8a428de97b3fa2637637
Julien Pellet <julien@netengine.com.au>
2013-03-19 15:43:17 +1000

    personnalized recipe

---
+++ site-cookbooks/julien/recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: julien
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+user_account 'deployer' do
+  # keys for file ~/.ssh/authorized keys
+  ssh_keys  ['paste your public ssh key here']
+end
+
+group "admin" do
+  members ['brandon', 'deployer']
+  action :create
+end
+
+group "sudo" do
+  members ['brandon', 'deployer']
+  action :create
+end
+
+execute "upgrade packages" do
+  action :run
+  command "apt-get -y upgrade"
+end
+
+execute "set up path for bash" do
+  command <<-EOC
+    sudo echo 'export PATH="/home/#{node['username']}/.rbenv/bin:$PATH"' >> /home/#{node['username']}/.bashrc
+    sudo echo 'eval "$(rbenv init -)"' >> /home/#{node['username']}/.bashrc
+  EOC
+  action :run
+end
