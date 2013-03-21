66b095d795b6bbad53cbe0863b068a9fa2ffa118
Julien Pellet <julien@netengine.com.au>
2013-03-21 13:17:59 +1000

    initialize rbenv for bash

--- site-cookbooks/julien/recipes/rbenv-setup.rb
+++ site-cookbooks/julien/recipes/rbenv-setup.rb
@@ -1,8 +1,21 @@
-rbenv_ruby "2.0.0-p0"
-
-rbenv_gem "bundler" do
-  ruby_version "2.0.0-p0"
-end
+#
+# Cookbook Name:: julien
+# Recipe:: rbenv-setup
+#
+# Author:: Julien Pellet (<contact@julienpellet.com>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 execute "set up path for bash" do
   command <<-EOC
